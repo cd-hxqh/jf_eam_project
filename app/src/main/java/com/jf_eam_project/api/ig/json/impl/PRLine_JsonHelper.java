b04c09b6c2dd2231bdcc46304e51df89d5c37105
taochao <278233503@qq.com>
2015-12-08 14:33:00 +0800

    采购计划查询功能12

--- app/src/main/java/com/jf_eam_project/api/ig/json/impl/PRLine_JsonHelper.java
+++ app/src/main/java/com/jf_eam_project/api/ig/json/impl/PRLine_JsonHelper.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.core.JsonToken;
 import com.instagram.common.json.JsonFactoryHolder;
 import com.jf_eam_project.api.ig.json.JsonHelper;
 import com.jf_eam_project.model.PR;
+import com.jf_eam_project.model.PRLine;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,24 +16,24 @@ import java.util.ArrayList;
  * 解析采购计划数据*
  */
 public final class PRLine_JsonHelper
-        implements JsonHelper<PR> {
-    private static final String TAG = "PR_JsonHelper";
+        implements JsonHelper<PRLine> {
+    private static final String TAG = "PRLine_JsonHelper";
 
     /**
      * 解析List*
      */
-    public static ArrayList<PR> parseFromJsonList(JsonParser jp)
+    public static ArrayList<PRLine> parseFromJsonList(JsonParser jp)
             throws IOException {
 
-        ArrayList<PR> results = null;
+        ArrayList<PRLine> results = null;
 
         // validate that we're on the right token
 
 
         if (jp.getCurrentToken() == JsonToken.START_ARRAY) {
-            results = new ArrayList<PR>();
+            results = new ArrayList<PRLine>();
             while (jp.nextToken() != JsonToken.END_ARRAY) {
-                PR parsed = parseFromJson(jp);
+                PRLine parsed = parseFromJson(jp);
                 Log.i(TAG, "parsed=" + parsed);
                 if (parsed != null) {
                     results.add(parsed);
@@ -46,11 +47,11 @@ public final class PRLine_JsonHelper
 
 
     /**
-     * 解析po
+     * 解析prline
      */
-    public static PR parseFromJson(JsonParser jp)
+    public static PRLine parseFromJson(JsonParser jp)
             throws IOException {
-        PR instance = new PR();
+        PRLine instance = new PRLine();
 
         // validate that we're on the right token
         if (jp.getCurrentToken() != JsonToken.START_OBJECT) {
@@ -68,45 +69,55 @@ public final class PRLine_JsonHelper
         return instance;
     }
 
-    public static boolean processSingleField(PR instance, String fieldName, JsonParser jp)
+    public static boolean processSingleField(PRLine instance, String fieldName, JsonParser jp)
             throws IOException {
-        if ("DESCRIPTION".equals(fieldName)) {
-            instance.description = jp.getValueAsString();
+        if ("CATEGORY".equals(fieldName)) {
+            instance.category = jp.getValueAsString();
             return true;
-        } else if ("ISSUEDATE".equals(fieldName)) {
-            instance.issuedate = jp.getValueAsString();
+        } else if ("CONVERSION".equals(fieldName)) {
+            instance.conversion = jp.getValueAsString();
+            return true;
+        } else if ("DESCRIPTION".equals(fieldName)) {
+            instance.description = jp.getValueAsString();
+        } else if ("ENTERBY".equals(fieldName)) {
+            instance.enterby = jp.getValueAsString();
+            return true;
+        } else if ("ENTERDATE".equals(fieldName)) {
+            instance.enterdate = jp.getValueAsString();
+            return true;
+        } else if ("ITEMNUM".equals(fieldName)) {
+            instance.itemnum = jp.getValueAsString();
+            return true;
+        } else if ("LINECOST".equals(fieldName)) {
+            instance.linecost = jp.getValueAsString();
+            return true;
+        } else if ("PRLINENUM".equals(fieldName)) {
+            instance.prlinenum = jp.getValueAsString();
             return true;
         } else if ("PRNUM".equals(fieldName)) {
             instance.prnum = jp.getValueAsString();
-        } else if ("REQUESTEDBY".equals(fieldName)) {
+            return true;
+        } else if ("TAX1".equals(fieldName)) {
+            instance.tax1 = jp.getValueAsString();
+            return true;
+        } else if ("ORDERUNIT".equals(fieldName)) {
+            instance.orderunit = jp.getValueAsString();
+            return true;
+        } else if ("ORDERQTY".equals(fieldName)) {
+            instance.orderqty = jp.getValueAsString();
+            return true;
+        } else if ("UNITCOST".equals(fieldName)) {
+            instance.unitcost = jp.getValueAsString();
+            return true;
+        }else if ("LINETYPE".equals(fieldName)) {
+            instance.linetype = jp.getValueAsString();
+            return true;
+        }else if ("STORELOC".equals(fieldName)) {
+            instance.storeloc = jp.getValueAsString();
+            return true;
+        }
+        else if ("REQUESTEDBY".equals(fieldName)) {
             instance.requestedby = jp.getValueAsString();
-            return true;
-        } else if ("REQUIREDDATE".equals(fieldName)) {
-            instance.requireddate = jp.getValueAsString();
-            return true;
-        } else if ("SITEID".equals(fieldName)) {
-            instance.siteid = jp.getValueAsString();
-            return true;
-        } else if ("STATUS".equals(fieldName)) {
-            instance.status = jp.getValueAsString();
-            return true;
-        } else if ("STATUSDATE".equals(fieldName)) {
-            instance.statusdate = jp.getValueAsString();
-            return true;
-        } else if ("UDPRTYPE".equals(fieldName)) {
-            instance.udprtype = jp.getValueAsString();
-            return true;
-        } else if ("PRETAXTOTAL".equals(fieldName)) {
-            instance.pretaxtotal = jp.getValueAsString();
-            return true;
-        } else if ("TOTALTAX1".equals(fieldName)) {
-            instance.totaltax1 = jp.getValueAsString();
-            return true;
-        } else if ("TOTALCOST".equals(fieldName)) {
-            instance.totalcost = jp.getValueAsString();
-            return true;
-        } else if ("CURRENCYCODE".equals(fieldName)) {
-            instance.currencycode = jp.getValueAsString();
             return true;
         }
 
@@ -117,7 +128,7 @@ public final class PRLine_JsonHelper
     /**
      * 解析PR_List*
      */
-    public static ArrayList<PR> parseFromJsonList(String inputString)
+    public static ArrayList<PRLine> parseFromJsonList(String inputString)
             throws IOException {
         JsonParser jp = JsonFactoryHolder.APP_FACTORY.createParser(inputString);
         jp.nextToken();
@@ -127,7 +138,7 @@ public final class PRLine_JsonHelper
     /**
      * 解析PR*
      */
-    public static PR parseFromJson(String inputString)
+    public static PRLine parseFromJson(String inputString)
             throws IOException {
         JsonParser jp = JsonFactoryHolder.APP_FACTORY.createParser(inputString);
         jp.nextToken();
